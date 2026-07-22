eca3b1441e672d54e3eeb98c2a221820764a06bc
danieluy <danielsosa.uy@gmail.com>
2026-07-22 10:39:58 -0300

    fix: markdown headers

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  allowedDevOrigins: ["192.168.1.36"],
+};
 
 export default withNextIntl(nextConfig);
